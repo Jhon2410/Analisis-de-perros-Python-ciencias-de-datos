82aa17e0297d9e8e74d7c4af56dc86e9ffab1206
jhon anderson <apxanderson02@gmail.com>
2022-07-24 13:33:14 -0500

    reemplzando cv2

--- img_procesor.py
+++ img_procesor.py
@@ -1,11 +1,12 @@
 from IPython import display
 from matplotlib import pyplot as plt
-import cv2
+import imageio as iio
+import tensorflow as tf
 
 class img_procesor():
 
     def procs_img(img_path: str):
-        img = cv2.imread(img_path)
+        img = iio.imread(img_path)
         display.display(display.Image(img_path))
         #recortar imagen central
         x,y,r = img.shape    # guardar tamaño de la imagen
@@ -29,8 +30,8 @@ class img_procesor():
         plt.show()
         
         # reescalado de imagen
-        img = cv2.resize(cropped_image,(224,224))   
-        img = img.reshape(1,224,224,3) 
+        img = tf.image.resize(img,(224,224))
+        img = tf.reshape(img, (1,224,224,3), name=None)
         
         
         return img
